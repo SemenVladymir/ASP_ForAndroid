03e632704f2e9df0e7c90e88788fe8df92fc8625
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2024-05-23 21:54:39 +0300

    Добавьте файлы проекта.

---
+++ Context/RegistrationContext.cs
@@ -0,0 +1,18 @@
+﻿using ASP_ForAndroid.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_ForAndroid.Context
+{
+    public class RegistrationContext : DbContext
+    {
+
+        public DbSet<User> Users { get; set; }
+        public RegistrationContext(DbContextOptions<RegistrationContext> options) : base(options) { }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-MV43C0T;Database=UserBase;Trusted_Connection=True;TrustServerCertificate=True;");
+        }
+    }
+}
---
+++ Controllers/RegistrationController.cs
@@ -0,0 +1,76 @@
+﻿using ASP_ForAndroid.Context;
+using ASP_ForAndroid.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ASP_ForAndroid.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistrationController : ControllerBase
+    {
+        private readonly RegistrationContext _dbContext;
+        public RegistrationController(RegistrationContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpPost]
+        [Route("Autorisation")]
+        public ActionResult ClientRegistration([FromBody] User newData)
+        {
+            try
+            {
+                if (_dbContext.Users.ToList().Any(e => e.Login == newData.Login))
+                    return new JsonResult("This login exists!");
+                else
+                {
+                    if (!string.IsNullOrEmpty(newData.Login) && !string.IsNullOrEmpty(newData.Password) && !string.IsNullOrEmpty(newData.Email))
+                    {
+                        _dbContext.Users.Add(newData);
+                        _dbContext.SaveChanges();
+                        return Ok("You have passed the registration verification!");
+                    }
+                    else
+                        return BadRequest("You haven`t passed the registration verification!");
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [HttpPost]
+        [Route("Verification")]
+        public ActionResult ClientVerificaation([FromBody] User newData)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(newData.Login) && !string.IsNullOrEmpty(newData.Password) && !string.IsNullOrEmpty(newData.Email))
+                {
+                    if (_dbContext.Users.ToList().Any(e => e.Login == newData.Login))
+                        return Unauthorized("This login exists!");
+                    return Ok("You have passed the registration verification!");
+                }
+                else
+                    return Unauthorized("You haven`t passed the registration verification!");
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public JsonResult GetUsers()
+        {
+            return new JsonResult(_dbContext.Users.ToList());
+        }
+    }
+}
